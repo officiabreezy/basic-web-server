a682859455882f7fa3e4c13dd0c908c994bdbbcd
officiabreezy <isaacsegun2055@gmail.com>
2024-07-04 09:03:00 -0700

    first comment

--- index.js
+++ index.js
@@ -4,20 +4,31 @@ const requestIp = require('request-ip');
 const dotenv = require('dotenv').config();
 const app = express();
 
+app.use(requestIp.mw());
+
 const port = process.env.PORT || 3000;
 
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'isaac';
-    const clientIp = req.clientIp || '127.0.0.0';
+    const clientIp = req.ip || '127.0.0.0';
+
 
     try{
-        const locationResponse = await axios.get(`http://ipinfo.io/${clientIp}/json`);
+        const locationResponse = await axios.get(`http://ipinfo.io/102.88.35.182/json?token=fb8dbb73e54520`);
         const locationData = locationResponse.data;
-        const city = locationData.city || 'abeokuta city';
+        const loc = locationResponse.data.loc
+        const [latitude,longitude] = loc.split(',');
+        const city = locationData.city || 'Ibadan city'
+        // const lat = locationResponse.data.lat
+        console.log(locationData);
+        console.log(loc);
+    
 
-        const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_KEY}&q=${city}`);
-        const weatherData = weatherResponse.data;
+        //const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_KEY}&q=${city}`);
+        const { data: weatherData } = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_KEY}&q=${loc}`)
+        console.log(weatherData);
+        //const weatherData = weatherResponse.data;
         const temperature = weatherData.current.temp_c;
 
         res.json({
